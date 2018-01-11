bea2c872710a55acfc29d01c45dea611439ed206
NyanCat12 <ldj.000000@gmail.com>
2018-01-11 17:55:12 +0800

    Add files via upload
    改成百度识别

--- hero.py
+++ hero.py
@@ -10,25 +10,24 @@ def million():
 	image_file = '11.jpg'
 	question_and_op = cropimage.crop(image_file)
 
-	question = question_and_op[0]
+	question_image = question_and_op[0]
 	op_a = question_and_op[1]
 	op_b = question_and_op[2]
 	op_c = question_and_op[3]
 
-	print ('question')
-	#map_.map_baidu(ocr.ocr(question))
+	print ('')
 
-	print ('op_a')
-	x = ocr.ocr(op_a)
-	print (x)
-	print (type(x))
-	print (len(x))
-
-	print ('op_b')
-	#print (ocr.ocr(op_b))
-
-	print ('op_c')
-	#print (ocr.ocr(op_c))
+	question = ocr.ocr(question_image)
+	op_a = ocr.ocr(op_a)
+	op_b = ocr.ocr(op_b)
+	op_c = ocr.ocr(op_c)
+	
+	print (question)
+	print (op_a)
+	print (op_b)
+	print (op_c)
+	
+	result = map_.map_baidu(question)
 
 	end = time.time()
 	print('程序用时：'+str(end-start)+'秒')
--- ocr.py
+++ ocr.py
@@ -1,13 +1,17 @@
 import urllib.request, sys, base64, json, os
+from aip import AipOcr
 
 def ocr(image):
+	'''
+	汉王ocr 涨价涨价了。。
 	host = 'http://text.aliapi.hanvon.com'
 	path = '/rt/ws/v1/ocr/text/recg'
 	method = 'POST'
-	appcode = '8b9a002246d6451c96cc5d0bc22b705c'    #汉王识别appcode（填你自己的）
+	appcode = 'a962e94260ee4043b824d2f40c126d8e'    #汉王识别appcode（填你自己的）
 	querys = 'code=74e51a88-41ec-413e-b162-bd031fe0407e'
 	bodys = {}
 	url = host + path + '?' + querys
+
 	f=open("D:\LDJ\misc\misc\MillionHero\MillionHeroes-master\crop\\"+image,'rb') 
 	ls_f=base64.b64encode(f.read())
 	f.close()
@@ -21,7 +25,6 @@ def ocr(image):
 	request.add_header('Content-Type', 'application/octet-stream')
 	response = urllib.request.urlopen(request)
 	content =  bytes.decode(response.read())
-
 	if (content):
 		decode_json = json.loads(content)
 		#print(decode_json['textResult'])
@@ -29,4 +32,22 @@ def ocr(image):
 
 	#pyperclip.copy(''.join(decode_json['textResult'].split()))
 	keyword = ''.join(decode_json['textResult'].split())    #识别的问题文本
-	return keyword
+
+	'''
+	""" （百度ocr）你的 APPID AK SK """
+	APP_ID = '10675766'
+	API_KEY = '3LXbCnaZ5TUx9T5hpzyP7pw2'
+	SECRET_KEY = 'TfKCxzZxjyTxHhOm1k2o2C5RCHiW52tl'
+	client = AipOcr(APP_ID, API_KEY, SECRET_KEY)
+
+	def get_file_content(filePath):
+		with open(filePath, 'rb') as fp:
+			return fp.read()
+	image = get_file_content(r"D:\LDJ\misc\misc\MillionHero\MillionHeroes-master\crop\\"+image)
+	respon = client.basicGeneral(image)
+	titles = respon['words_result']          #获取问题
+	ans = ''
+	for title in titles:
+		  ans = ans +title['words']
+
+	return ans
