f795c0bba5c13e25090e1c89e1899e461600b1b8
cloisonne <tylerluan@163.com>
2018-01-11 18:22:55 +0800

    final push

--- baiduSearch/search.py
+++ baiduSearch/search.py
@@ -6,9 +6,5 @@ from . import process
 def search(keyword, **kwargs):
     kwargs.setdefault('convey', False)
     page = get.page(keyword)
-    #page.encoding = page.apparent_encoding
-    results = process.page(page)
-    if kwargs['convey']:
-        for result in results:
-            result.convey_url()
-    return results
+    process.page(page)
+    return
